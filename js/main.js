50274626cd0584fe32db590882d7b7b6e3efba3b
aguba <keyblade_0@hotmail.com>
2017-04-29 19:00:04 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -163,7 +163,7 @@ var createTextureMaterial = function (textureName) {
         normalName = textureName.slice(0, textureName.indexOf('.')) + normStr;
     }
 
-    loader.setPath('../images/');
+    loader.setPath('https://github.com/aguba/GraphicsFinal/tree/master/images/');
 
     var texture = loader.load(textureName);
     var normal = loader.load(normalName);
